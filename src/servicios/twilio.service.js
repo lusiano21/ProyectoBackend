4958ac586713edb2480bed35f271517f52539e8d
lusiano21 <lusianobonifacio6@gmail.com>
2023-08-12 11:33:56 -0300

    Proyecto final 1.2.5

--- src/servicios/twilio.service.js
+++ src/servicios/twilio.service.js
@@ -1,5 +1,5 @@
 import twilio from 'twilio'
-import config from '../config/config.js'
+
 class TwilioService {
     constructor() {
         this.client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)

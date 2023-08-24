cc9a0dc95b48b0991ac9a4e937ed5a831891ef95
lusiano21 <lusianobonifacio6@gmail.com>
2023-08-23 23:39:18 -0300

    Proyecto final 3.3.4

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -156,9 +156,10 @@ export const resolve = async (id, body) => {
     if (!business) {
       throw new NotFoundException('Products not found')
     }
-    const product = order.products.map((product) => product)
-    console.log('Me deja Entrar al array', product.quantity)
-    if(business.stock >= product){
+    const product = order.products.map((product) => product.quantity)
+    const [a] = product
+    console.log('Me deja Entrar al array', product)
+    if(business.stock >= a){
       const subtotal = order.products.reduce((acc, product) => {
         return acc + (business.stock - product.quantity)
       }, 0)

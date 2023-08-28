ecea22af91ea70a5723b8e9688e5a51407447c1c
lusiano21 <lusianobonifacio6@gmail.com>
2023-08-28 18:59:29 -0300

    Proyecto final 3.7.8

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -4,8 +4,14 @@
     .then(res => res.json())
     .then(data => {
         if(data.success){
+            if(data.payload.rol){
+                btnBuy.innerHTML = `
+                <a href="#" class="btn btn-outline-primary" type="button">Comprar</a>
+                `
+                }
+        } else {
             btnBuy.innerHTML = `
-            <a href="#" class="btn btn-outline-primary" type="button">Comprar</a>
+            <a href="#" class="btn btn-outline-primary" type="button">Ver</a>
             `
         }
     })

0a8a394a5886496cbbdcdff7dd9a9d8db0f57509
lusiano21 <lusianobonifacio6@gmail.com>
2023-08-29 14:32:30 -0300

    Proyecto final 3.10.1

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -64,6 +64,21 @@
         })
         valueTotalList.innerHTML = calculoTotal() + "$"
     }
+    fetch('/api/sessions/me')
+    .then(res => res.json())
+    .then(data => {
+        if(data.success){
+            if(data.payload.rol){
+                trolleyList.innerHTML = `
+                <a href="#" class="btn btn-outline-primary" type="button">Comprar</a>
+                `
+                }
+        } else {
+            trolleyList.innerHTML = `
+            <p>No puedes realizar compras hasta que estes registrado</p>
+            `
+        }
+    })
     fetch('/')
         .then(res => res.json())
         .then(data => {
@@ -122,33 +137,5 @@
             `
         }
     })
-    const usuarios = document.getElementById('main')
-  const page = document.getElementById('page')
-  let site = '/api'
-  fetch(site)
-  .then(res => res.json())
-  .then(data => {
-    data.payload.forEach(element => {
-      let business = document.createElement("article");
-      business.className = "card";
-      let businessName = document.createElement("h2");
-      businessName.innerHTML= `${element.name}`;
-      let card = document.createElement("div");
-      card.className = "card"; 
-      for(const product of element.products){
-        let menu = document.createElement("h3");
-        menu.innerHTML= `Menu ${product.id}:`;
-        let name_product = document.createElement("h4");
-        name_product.innerHTML=`${product.name}`
-        let price_product = document.createElement("h4");
-        price_product.innerHTML=`Precio: $${product.price}`
-
-        card.append(menu);
-        card.append(name_product);
-        card.append(price_product)
-      }
-      business.append(businessName)
-      business.append(card)
-      usuarios.append(business)
     });
   })*/
